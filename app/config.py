10e35780faebac3bd122119c487665dda2a27ce8
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2025-08-21 00:39:39 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,26 +1,15 @@
 import os
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 load_dotenv()
 
-# Proxy setup
-PROXY = {
-    "http": f"http://{os.getenv('PROXY_USER')}:{os.getenv('PROXY_PASS')}@{os.getenv('PROXY_DOMAIN')}:{os.getenv('PROXY_PORT')}",
-    "https": f"http://{os.getenv('PROXY_USER')}:{os.getenv('PROXY_PASS')}@{os.getenv('PROXY_DOMAIN')}:{os.getenv('PROXY_PORT')}",
-}
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME", "deadline_api")
+API_KEY = os.getenv("API_KEY")  # baad me generate karke mongo me daalna
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
 
-# Paths
-COOKIES_PATH = os.getenv("COOKIES_PATH", "cookies/cookies.txt")
-DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "downloads")
-
-# Plans => daily_limit, monthly_limit
-API_KEYS = {
-    "FREEKEY123": (1000, 30000),
-    "BASICKEY456": (5000, 150000),
-    "PROKEY789": (10000, 300000),
-    "ULTRAKEY000": (20000, 600000),
-    "MEGAKEY111": (30000, 900000),
-    "SUPERKEY222": (50000, 1500000),
-    "MAXKEY333": (100000, 3000000),
-    "GODKEY444": (150000, 4500000),
-}
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+users_col = db["users"]
